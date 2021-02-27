0fefb2607f3ae5c66a1550bbad133f771ca76d81
laljbour <61568319+laljbour@users.noreply.github.com>
2021-02-27 00:06:38 -0700

    Added functionality to addNumber and reverseNumber and created reverseNum variable

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Lawrence Aljbour
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,6 +14,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        this.previousNumber += num;
         System.out.print("Add num to previousNumber");
         return 0;
     }
@@ -46,6 +48,7 @@ public class GitDemo {
             newNumber = newNumber * i;
         }
         
+        reverseNum = newNumber;
         return newNumber;
     }
     
